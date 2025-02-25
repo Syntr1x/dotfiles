b07536294fad5a61e32c64dc197295ecdf4527c3
Maris <124806561+Syntr1x@users.noreply.github.com>
2025-02-25 22:45:37 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -61,6 +61,7 @@ install_yay_packages
 copy_configs
 enable_network_manager
 install_browser
+
 echo "Cleaning up..."
 rm -rf /home/$USER/hyprconf.syn /home/$USER/tempconf /home/$USER/.config/install.sh /home/$USER/.config/README.md /home/$USER/.config/LICENSE
 echo "Installation complete. Please restart your session for the changes to take effect."
