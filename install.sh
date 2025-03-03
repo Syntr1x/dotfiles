1222354a391bae493ba0e18567a4abb4d0598edf
Maris <124806561+Syntr1x@users.noreply.github.com>
2025-03-03 22:25:20 +0100

    Update install.sh
    clean up

--- install.sh
+++ install.sh
@@ -52,5 +52,5 @@ copy_configs
 enable_network_manager
 install_browser
 install_sddm_astronaut_theme
-echo "Cleaning up..."; rm -rf /home/$USER/hyprconf.syn /home/$USER/tempconf /home/$USER/.config/install.sh /home/$USER/.config/README.md /home/$USER/.config/LICENSE
+echo "Cleaning up..."; rm -rf /home/$USER/hyprconf.syn /home/$USER/tempconf /home/$USER/.config/install.sh /home/$USER/.config/README.md /home/$USER/.config/LICENSE /home/$USER/.config/Ghostty-themes /home/$USER/.config/Rofi-themes
 echo "Installation complete. Please restart your session."
