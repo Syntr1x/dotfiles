06238ecc7c0ed6f1bb085a09c02098c7c0833d3b
Maris <124806561+Syntr1x@users.noreply.github.com>
2025-03-12 17:57:15 +0100

    Update install.sh
    used a f****** . wrong

--- install.sh
+++ install.sh
@@ -56,7 +56,7 @@ install_sddm_astronaut_theme
 # Run theme selection script 
 echo "Running theme selection script..."
 sudo chmod +x /home/$USER/tempconf/themeselect.sh
-./home/$USER/tempconf/themeselect.sh
+/home/$USER/tempconf/themeselect.sh
 
 echo "Cleaning up..."; sudo rm -rf /home/$USER/hyprconf.syn /home/$USER/tempconf /home/$USER/.config/install.sh /home/$USER/.config/README.md /home/$USER/.config/LICENSE /home/$USER/.config/Ghostty-themes /home/$USER/.config/Rofi-themes
 echo "Installation complete. Please restart your session."
