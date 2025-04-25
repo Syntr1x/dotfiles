bb50aa4e78cba878e31132edc5a68eff92116bf6
Maris <124806561+Syntr1x@users.noreply.github.com>
2025-04-25 21:29:20 +0200

    Update bluetooth.sh

--- hypr/scripts/bluetooth.sh
+++ hypr/scripts/bluetooth.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 bluetoothctl << EQF
-pair 7C:66:EF:4A:1E:0C #MAC Adress from PS5 Controller
-connect 7C:66:EF:4A:1E:0C
+pair  #MAC Adress from controller or other bluetooth device
+connect  #same here
 EQF
 
 read -p "If you want to disconnect just press Enter..." disconnect
