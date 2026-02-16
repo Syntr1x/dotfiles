bdb9e39ec5f394c11a13e840f65758ae3671a458
Maris <124806561+Syntr1x@users.noreply.github.com>
2026-02-16 19:03:42 +0100

    Add green theme and corresponding wallpapers

--- hypr/scripts/themeselect.sh
+++ hypr/scripts/themeselect.sh
@@ -8,9 +8,9 @@ waybar_css="$config_dir/waybar/style.css"
 wallpaper_dir="$config_dir/hypr/Wallpapers"
 reload_script="$config_dir/hypr/scripts/reload-hyprpaper.sh"
 
-themes=("syn-beige" "syn-Broadcast" "syn-mellow" "syn-Ocean" "IC_Orange_PPL" "Gruvbox" "syn-rose-pine" "syn-Tango" "Tomorrow")
-theme_names=("Beige" "Dark" "Purple" "Blue" "Orange" "Gruvbox" "Kirby" "Moondrop(white/Red)" "Winter")
-wallpapers=("Flowers.png" "darkPlants.jpg" "pinkRose.jpg" "bluesky.jpg" "TrainPath.png" "Arch_retro.png" "kirby.jpg" "Moondrop_white.jpg" "winter.jpg")
+themes=("syn-beige" "syn-Broadcast" "syn-mellow" "syn-Ocean" "IC_Orange_PPL" "Gruvbox" "syn-rose-pine" "syn-Tango" "Tomorrow" "syn-green")
+theme_names=("Beige" "Dark" "Purple" "Blue" "Orange" "Gruvbox" "Kirby" "Moondrop(white/Red)" "Winter" "Green" )
+wallpapers=("Flowers.png" "darkPlants.jpg" "pinkRose.jpg" "bluesky.jpg" "TrainPath.png" "Arch_retro.png" "kirby.jpg" "Moondrop_white.jpg" "winter.jpg" "leaves.jpg")
 rofi_themes=(
   "/usr/share/rofi/themes/rounded-beige.rasi"
   "/usr/share/rofi/themes/rounded-dark.rasi"
@@ -21,8 +21,9 @@ rofi_themes=(
   "/usr/share/rofi/themes/rounded-kirby.rasi"
   "/usr/share/rofi/themes/rounded-white.rasi"
   "/usr/share/rofi/themes/rounded-winter.rasi"
+  "/usr/share/rofi/themes/rounded-green.rasi"
 )
-waybar_colors=("#d8c8b3" "#888888" "#f0a0c0" "#a1cdf3" "#fed79d" "#d8c8b3" "#fdcbe6" "#888888" "#888888")
+waybar_colors=("#d8c8b3" "#888888" "#f0a0c0" "#a1cdf3" "#fed79d" "#d8c8b3" "#fdcbe6" "#888888" "#888888" "#8c9180")
 
 # ------------ STYLE ------------
 BOLD=$(tput bold)
