9eda73b00f080ac085af1eb10352ee31dde5739b
Maris <124806561+Syntr1x@users.noreply.github.com>
2026-06-05 15:49:24 +0200

    Add copying of wallpapers to SDDM backgrounds

--- install.sh
+++ install.sh
@@ -26,6 +26,7 @@ copy_configs() {
   sudo cp /home/$USER/tempconf/.bashrc /home/$USER/
   sudo cp /home/$USER/tempconf/themeselector.desktop /usr/share/applications/
   sudo cp /home/$USER/tempconf/defaultsyn.conf /usr/share/sddm/themes/silent/configs/
+  sudo cp /home/$USER/tempconf/hypr/Wallpapers/* /usr/share/sddm/themes/silent/backgrounds/
   sudo chown -R "$USER":"$USER" ~/.config/rofi
 }
 # Enable NetworkManager
