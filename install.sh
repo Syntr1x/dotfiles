7e8843b128d31fa8c7ed22e3fce7bd3ad5709fe2
Maris <124806561+Syntr1x@users.noreply.github.com>
2026-09-08 02:58:38 +0200

    Add zen-browser.desktop to application directory
    Added command to copy zen-browser.desktop to applications.

--- install.sh
+++ install.sh
@@ -31,6 +31,7 @@ copy_configs() {
   sudo cp /home/$USER/tempconf/Ghostty-themes/* /usr/share/ghostty/themes 2>/dev/null
   sudo cp /home/$USER/tempconf/.bashrc /home/$USER/
   sudo cp /home/$USER/tempconf/themeselector.desktop /usr/share/applications/
+  sudo cp /home/$USER/tempconf/zen-browser.desktop /usr/share/applications/
   sudo cp /home/$USER/tempconf/defaultsyn.conf /usr/share/sddm/themes/silent/configs/ 2>/dev/null
   sudo cp /home/$USER/tempconf/hypr/Wallpapers/* /usr/share/sddm/themes/silent/backgrounds/ 2>/dev/null
   sudo chown -R "$USER":"$USER" /usr/share/sddm/themes/silent/configs/
